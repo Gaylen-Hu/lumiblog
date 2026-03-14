5f0b4327efa83c00a97f94523375b3b4ec7ccef8
xinyuHu <hxyrkcy@outlook.com>
2026-03-14 23:34:43 +0800

    1

--- apps/blog-web/blog_web/src/middleware.ts
+++ apps/blog-web/blog_web/src/middleware.ts
@@ -5,7 +5,7 @@ export default createMiddleware(routing)
 
 export const config = {
   matcher: [
-    // 匹配所有路径，排除 _next、api、静态文件
-    '/((?!_next|api|.*\\..*).*)',
+    // 匹配所有路径，排除 _next、api、静态文件、badmin
+    '/((?!_next|api|badmin|.*\\..*).*)',
   ],
 }
